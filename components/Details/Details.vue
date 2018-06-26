52f7667e8111df4a85dadc15d70672592c58b416
mktcode <kontakt@markus-kottlaender.de>
2018-06-26 17:07:51 +0200

    added VoteControl component

--- components/Details/Details.vue
+++ components/Details/Details.vue
@@ -1,2 +1,12 @@
+<script>
+  import VoteControl from '~/components/VoteControl/VoteControl'
+
+  export default {
+    components: {
+      VoteControl
+    }
+  }
+</script>
+
 <template src="./template.html"></template>
 <style src="./style.sass" lang="sass"></style>
